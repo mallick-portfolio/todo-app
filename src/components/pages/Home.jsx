238ea04b1bc624d8dc1aad4de91e82989a8cefaf
Tamal Mallick <tamal1714019@gmail.com>
2022-03-20 23:28:28 +0600

    Start todo app

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button, Container } from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <Container>
+      <div>
+        <h1>home component</h1>
+        <Button variant="info">Hello world</Button>
+      </div>
+    </Container>
+  );
+};
+
+export default Home;
